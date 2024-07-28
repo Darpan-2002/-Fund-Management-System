39a3a224e0e301cd345f8a55651aca84ddf3975e
Priyajit27 <priyajitpaul27@gmail.com>
2024-07-28 10:28:14 +0530

    Contact,team,about page and section links added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,29 @@
-import React from 'react'
+// src/About.js
+
+import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-container">
+      <img
+        src="https://res.cloudinary.com/priyojit/image/upload/v1722105037/WhatsApp_Image_2024-07-27_at_11.55.08_PM_kfcvcp.jpg"
+        alt="About Us"
+        className="about-photo"
+      />
+      <div className="about-text">
+        <h2>About Us</h2>
+        <p>
+          Welcome to our company! We are dedicated to providing the best services
+          to our clients. Our team is composed of highly skilled professionals
+          who are passionate about their work. We believe in continuous
+          improvement and strive to exceed expectations in every project we
+          undertake. Thank you for visiting our website, and we look forward to
+          working with you.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbarnew from '../Navbarnew/Navbarnew';
-
+import SectionLinks from '../../SectionLinks/SectionLinks';
+import "./Home.css"
 const Home = () => {
   return (
    <div className="content">
     <Navbarnew/>
         <h1>Welcome to Fund Management website</h1>
         <p>A blockchain-based fund management system leverages the decentralized, transparent, and secure nature of blockchain technology to revolutionize the management of investment funds. By utilizing smart contracts, the system ensures that all transactions and fund allocations are executed automatically based on predefined rules, reducing the need for intermediaries and minimizing the risk of human error. Investors benefit from enhanced transparency as all transactions are recorded on the blockchain, allowing for real-time auditing and verification. The immutable ledger also provides increased security against fraud and tampering.</p>
+
         <div className='hello' > 
-          <img src="https://res.cloudinary.com/priyojit/image/upload/v1713957627/vjxexasxhrothofjip4r.png" alt=""  width={250} height={250}/>
+          <img src="https://res.cloudinary.com/priyojit/image/upload/v1722106955/images_wc2ngj.jpg" alt=""  width={250} height={250}  className='hello1' />
           </div> 
+          <SectionLinks />
        </div>
   );
 };
